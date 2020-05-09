3c1763fcb06846a038dcabf7faeeefb1eddd8f25
Abhishek Gangwar <Abhishek.Gangwar@gartner.com>
2020-05-09 22:17:44 +0530

    Initial setup for Microfrontend

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MicroFrontEndTwo() {
+  return /*#__PURE__*/React.createElement("div", null, " This is micro frontend two");
+}
+
+export default MicroFrontEndTwo;
---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MicroFrontEndTwo() {
+  return <div> This is micro frontend two</div>;
+}
+
+export default MicroFrontEndTwo;
